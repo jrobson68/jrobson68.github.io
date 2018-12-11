f4013757773c996342af8a3c9fd4d0a84c79a82a
Joshua <jrobson68@hotmail.co.uk>
2018-12-11 14:18:28 +0000

    Choor door step 41

--- subpages/webDev/projects/choreDoor/choreDoor.js
+++ subpages/webDev/projects/choreDoor/choreDoor.js
@@ -4,6 +4,10 @@ var doorImage3 = document.getElementById('door3');
 var botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 var beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 var spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+var numClosedDoors = 3;
+var openDoor1;
+var openDoor2;
+var openDoor3;
 
 door1.onclick = () => {
 	doorImage1.src = botDoorPath;
@@ -15,4 +19,16 @@ door2.onclick = () => {
 
 door3.onclick = () => {
 	doorImage3.src = spaceDoorPath;
+}
+
+const randomChoreDoorGenerator = () => {
+	var choreDoor = Math.floor(Math.random() * numClosedDoors);
+	
+	if(choreDoor === 0) {
+		
+	} else if(choreDoor === 1) {
+		
+	} else {
+		
+	}
 }
