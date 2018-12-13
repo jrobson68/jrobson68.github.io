eb0d79ad3a74fa2ab9258efc21069c1357df30da
Joshua <jrobson68@hotmail.co.uk>
2018-12-13 09:55:11 +0000

    Piano Keys up to step 6

--- subpages/webDev/projects/pianoKeys/pianoKeys.js
+++ subpages/webDev/projects/pianoKeys/pianoKeys.js
@@ -6,13 +6,26 @@ keys.forEach(function(key){
 })
 
 // Write named functions that change the color of the keys below
-
+const keyPlay = function(event) {
+	event.target.style.backgroundColor = '00cc00';
+}
+	
+const keyReturn = function(event) {
+	event.target.style.backgroundColor = '';
+}
 
 // Write a named function with event handler properties
-
+const eventAssignment = function(note) {
+	note.onmousedown = function() {
+		keyPlay(event);
+	}
+	note.onmouseup = function() {
+		keyReturn(event);
+	}
+}
 
 // Write a loop that runs the array elements through the function
-
+notes.forEach(eventAssignment);
 
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
